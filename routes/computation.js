e0b998a194453828ee316d5ffe660869b04b42a7
S567541 <S567541@nwmissouri.edu>
2023-10-25 14:35:33 -0500

    computaion

--- routes/computation.js
+++ routes/computation.js
@@ -3,10 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+    random=Math.random();
     log=Math.log(random)
   res.send(`Log function is appilied ${random} is ${log}`);
 });
 
 module.exports = router;
-var random=Math.random();
+var random=0;
 var log;
